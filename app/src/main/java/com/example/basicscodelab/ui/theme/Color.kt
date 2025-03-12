84857da98268c787139a5c5ade0921423fb779fe
Billy Jonathan <46766058+Inventiner@users.noreply.github.com>
2025-03-12 15:18:14 +0700

    Feat: Init Repo

---
+++ app/src/main/java/com/example/basicscodelab/ui/theme/Color.kt
@@ -0,0 +1,13 @@
+    package com.example.basicscodelab.ui.theme
+
+    import androidx.compose.ui.graphics.Color
+
+    val DeepTeal = Color(0xFF005f73)
+    val TealGreen = Color(0xFF0a9396)
+    val White = Color.White
+    val WarmGray = Color(0xFF99958d)
+    val LightGray = Color(0xFFe9d8a6)
+    val CoralRed = Color(0xFFee9b00)
+    val DarkPlum = Color(0xFF330725)
+    val DarkOlive = Color(0xFF303607)
+    val SoftYellow = Color(0xFFffffc2)
